c9fcff0f2230eb0b05d77369fc8c68a55fde1ea7
Gabor Racz <gabzracz.dev@gmail.com>
2024-07-30 22:08:13 +0200

    clean

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,23 +1,3 @@
---return {
---  "kdheepak/lazygit.nvim",
---  dependencies = {
---    "nvim-lua/plenary.nvim",
---  },
---  config = function()
---    keys = {
---      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
---  end
-  --  cmd = {
-  --  "LazyGit",
-  --    "LazyGitConfig",
-  --    "LazyGitCurrentFile",
-  --    "LazyGitFilter",
-  --    "LazyGitFilterCurrentFile",
-  -- },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
--- }
-
 return {
   'kdheepak/lazygit.nvim',
   dependencies = {
